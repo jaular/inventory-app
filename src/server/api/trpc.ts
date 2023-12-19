61a32f11946833f056d69cf58e5fd9e75bbd5ace
Jose Aular <jose14aular@gmail.com>
2023-12-19 16:18:40 -0400

    Throw error

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -110,13 +110,12 @@ export const publicProcedure = t.procedure;
 /** Reusable middleware that enforces users are logged in before running the procedure. */
 const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
   if (!ctx.session?.user) {
-    // throw new TRPCError({ code: "UNAUTHORIZED" });
-    console.log("UNAUTHORIZED");
+    throw new TRPCError({ code: "UNAUTHORIZED" });
   }
   return next({
     ctx: {
       // infers the `session` as non-nullable
-      session: { ...ctx.session, user: ctx.session?.user },
+      session: { ...ctx.session, user: ctx.session.user },
     },
   });
 });
