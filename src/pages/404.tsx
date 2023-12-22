71bc8c94e367f58106aea37a481c4f2e74daa44e
Jose Aular <jose14aular@gmail.com>
2023-12-22 09:19:33 -0400

    Custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import Container from "~/components/Container";
+
+export default function Custom404() {
+  return (
+    <Container>
+      <h1 className="text-center text-lg font-medium md:text-2xl">
+        404 - P&aacute;gina no encontrada
+      </h1>
+    </Container>
+  );
+}
